423d31a477b92c9ec71a11709bafec39f97bf5ca
sagar subedi <sagar.subedi.1884@gmail.com>
2022-04-13 14:44:25 +0545

    some changes on js

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const generateBtn = document.querySelector(".btn");
 const qrInput = document.getElementById("inp");
 
 generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   let qrValue = qrInput.value;
   if (!qrValue) return;
-
   const image = document.createElement("img");
-  image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
-  document.querySelector(".container").appendChild(image);
+  // image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  document.querySelector(
+    ".qr"
+  ).children[0].innerHTML = `<img src=https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}/>`;
+
+  // console.log(document.querySelector(".qr").children[0]);
 });
